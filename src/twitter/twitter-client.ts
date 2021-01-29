d53c0a2f466a8336b21d600a62ab07724906eb8c
gvdp <vdputteglenn@gmail.com>
2021-01-29 22:29:37 +0100

    disable twitter calls

--- src/twitter/twitter-client.ts
+++ src/twitter/twitter-client.ts
@@ -7,13 +7,14 @@ const twitterApi = axios.create({
 
 export async function getLatestTweet() {
 	const twitterBotName = 'covidvaccins_be'
-	const listResponse = await twitterApi.get('/1.1/statuses/user_timeline.json?screen_name=' + twitterBotName).then(resp => resp.data)
+	// const listResponse = await twitterApi.get('/1.1/statuses/user_timeline.json?screen_name=' + twitterBotName).then(resp => resp.data)
+	const listResponse = [{text: '█░░░░░░░░░░░░░░░░░░░░░░░ 2.321%'}]
 	console.log(listResponse)
 	return listResponse[0].text
 }
 
 export async function tweet(tweetText: string) {
 	console.log('Tweeting', tweetText)
-	await twitterApi.post('/1.1/statuses/update.json', null, {params: {status: tweetText}})
+	// await twitterApi.post('/1.1/statuses/update.json', null, {params: {status: tweetText}})
 	console.log('Tweeted')
 }
